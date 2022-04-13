164a125b2b79d3b8a99982712eb7a9b36d0a8480
Krzysztof Pęczek <krzysztofpeczek@protonmail.com>
2022-04-13 19:08:18 +0200

    Added printing longest increasing subsequence

--- practice/problems/lis.py
+++ practice/problems/lis.py
@@ -7,8 +7,31 @@ def lis(array):
 
     for i in range(1, n):
         for j in range(0, i):
-            if array[i] > array[j]:
-                F[i] = max(F[i], F[j] + 1)
+            if array[i] > array[j] and F[j] + 1 > F[i]:
+                F[i] = F[j] + 1
         if F[i] > F[max_index]:
             max_index = i
     return F[max_index]
+
+
+def print_lis(array):
+    n = len(array)
+    F = [1 for i in range(n)] # Stores values of lis untill index i
+    max_index = 0
+
+    parent = [-1 for i in range(n)]
+
+    for i in range(1, n):
+        for j in range(0, i):
+            if array[i] > array[j] and F[j] + 1 > F[i]:
+                F[i] = F[j] + 1
+                parent[i] = j
+        if F[i] > F[max_index]:
+            max_index = i
+
+    print_sol(array, parent, max_index)
+
+def print_sol(array, parent, index):
+    if parent[index] != -1:
+        print_sol(array, parent, parent[index])
+    print(array[index])
